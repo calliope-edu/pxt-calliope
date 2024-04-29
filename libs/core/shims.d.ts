d6668e3bddce0abd292324ad6caef91994deba3d
Juri Wolf <Amerlander@users.noreply.github.com>
2024-04-29 13:08:56 +0200

    enums

--- libs/core/shims.d.ts
+++ libs/core/shims.d.ts
@@ -1280,6 +1280,58 @@ declare namespace input {
     //% help="input/logo-is-pressed" shim=input::logoIsPressed
     function logoIsPressed(): boolean;
 }
+
+
+    /**
+     * Provides access to basic calliope mini functionality.
+     */
+
+declare namespace basic {
+
+    /**
+     * Sets the color on the built-in RGB LED. Set to 0 to turn off.
+     */
+    //% blockId=device_turn_rgb_led_off block="turn built-in LED off"
+    //% help=basic/turn-rgb-led-off
+    //% weight=10
+    //% group="RGB LED"
+    //% advanced=true shim=basic::turnRgbLedOff
+    function turnRgbLedOff(): void;
+
+    /**
+     * Sets the color on the built-in RGB LED. Set to 0 to turn off.
+     * @param color1 The color of the first LED in RGB format (e.g., 0xFF0000 for red).
+     */
+    //% blockId=device_set_led_colors-dal
+    //% hidden=1 shim=basic::setLedColorDal
+    function setLedColorDal(color: int32): void;
+
+    /**
+     * Sets the color on the built-in RGB LED. Set to 0 to turn off.
+     * @param color1 The color of the first LED in RGB format (e.g., 0xFF0000 for red).
+     * @param color2 The second LED color.
+     * @param color3 The third LED color.
+     * @param brightness The LED brightness in percent.
+     */
+    //% blockId=device_set_led_colors-codal brightness.defl=20 shim=basic::setLedColorsCodal
+    function setLedColorsCodal(color1: int32, color2: int32, color3: int32, brightness?: int32): void;
+
+    /**
+     * Sets the color on the built-in RGB LED. Set to 0 to turn off.
+     * @param color The color of the LED in RGB format (e.g., 0xFF0000 for red).
+     */
+    //% help=basic/set-led-color
+    //% blockId=device_set_led_color
+    //% block="set LED to %color=colorNumberPicker"
+    //% expandableArgumentMode="toggle"
+    //%
+    //%
+    //%
+    //%
+    //% weight=10
+    //% group="RGB LED" color.defl=0xff0000 shim=basic::setLedColor
+    function setLedColor(color?: int32): void;
+}
 declare namespace pins {
 
     /**
